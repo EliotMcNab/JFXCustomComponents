f63453dbc91a7826731625f021f9870a8e0abe8c
eliot <eliot.mcnab@laposte.net>
2022-03-16 14:19:57 +0100

    Finished commenting & added diagram for BorderLine

--- src/main/java/app/customControls/controls/corner/BorderLine.java
+++ src/main/java/app/customControls/controls/corner/BorderLine.java
@@ -11,18 +11,37 @@ import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * A visual display signaling a corner or an edge<br><br>
  * <u><i>corner types</i></u> :
  * TOP_LEFT, TOP, TOP_RIGHT, RIGHT, BOTTOM_RIGHT, BOTTOM, BOTTOM_LEFT, LEFT<br><br>
- * <u><i>css properties</i></u> :<br>
- * <list>
- *     <li>-fx-size: borderline width & height</li>
- *     <li>-fx-line-thickness: line thickness</li>
- *     <li>-fx-arc-width/-fx-arc-height: rounding for corners</li>
- *     <li>-fx-fill: fill color for the line</li>
- * </list>
+ * <u><i>CSS Pseudo-class</i></u> : border-line<br>
+ * <br>
+ * <u><i>CSS Properties</i></u> :<br>
+ * <ul>
+ *     <li>
+ *         -fx-size: number<br>
+ *         the width & height of the borderline
+ *     </li>
+ *     <li>
+ *         -fx-line-thickness: number<br>
+ *         the thickness of the line
+ *     </li>
+ *     <li>
+ *         -fx-arc-width: number<br>
+ *         arc-width for the side of the corner
+ *     </li>
+ *     <li>
+ *         -fx-arc-height: number<br>
+ *         arc-height for the side of the corner
+ *     </li>
+ *     <li>
+ *         -fx-fill: color<br>
+ *         the color of the corner
+ *     </li>
+ * </ul>
  */
 public class BorderLine extends Region {
 
@@ -31,11 +50,12 @@ public class BorderLine extends Region {
     // ===================================
 
     /*          DEFAULT VALUES          */
-    public static final double DEFAULT_SIZE        = 50;
-    public static final double DEFAULT_THICKNESS   = 10;
-    public static final double DEFAULT_ARC_WIDTH   = 0;
-    public static final double DEFAULT_ARC_HEIGHT  = 0;
-    public static final Color  DEFAULT_COLOR       = Color.BLACK;
+
+    public static final double DEFAULT_SIZE = 50;
+    public static final double DEFAULT_THICKNESS = 10;
+    public static final double DEFAULT_ARC_WIDTH = 0;
+    public static final double DEFAULT_ARC_HEIGHT = 0;
+    public static final Color DEFAULT_COLOR = Color.BLACK;
 
     /*     CSS STYLEABLE PROPERTIES     */
     private static final StyleablePropertyFactory<BorderLine> FACTORY =
@@ -80,7 +100,7 @@ public class BorderLine extends Region {
     private Point2D[] rectangleCoordinates;
 
     // orientation
-    private ObjectProperty<Orientation> orientation;
+    private final ObjectProperty<Orientation> orientation;
 
     /*             LISTENERS            */
 
@@ -135,8 +155,6 @@ public class BorderLine extends Region {
         this.arcWidthListener = (observableValue, oldArc, newArc)             -> setArcWidth(newArc.doubleValue());
         this.archHeightListener = (observableValue, oldArc, newArc)             -> setArcHeight(newArc.doubleValue());
         this.colorListener = (observableValue, oldColor, newColor)         -> setColor(newColor);
-
-        SimpleDoubleProperty test = new SimpleDoubleProperty();
 
         // saves the rectangle's format
         setSize(size);
@@ -266,6 +284,9 @@ public class BorderLine extends Region {
 
     }
 
+    /**
+     * Resets the {@link BorderLine} canvas
+     */
     private void clearDisplay() {
         context.clearRect(0, 0, getSize(), getSize());
     }
@@ -278,14 +299,26 @@ public class BorderLine extends Region {
         return getArcHeight() != 0 || getArcWidth() != 0;
     }
 
+    /**
+     * Determines whether to draw the horizontal rectangle making up the {@link BorderLine}
+     * @return (boolean): whether to draw the Borderline's horizontal rectangle
+     */
     private boolean shouldDrawHorizontal() {
         return isHorizontal() || isCorner();
     }
 
+    /**
+     * Determines whether to draw the vertical rectangle making up the {@link BorderLine}
+     * @return (boolean): whether to draw the BorderLine's vertical rectangle
+     */
     private boolean shouldDrawVertical() {
         return isVertical() || isCorner();
     }
 
+    /**
+     * Determines if the {@link BorderLine} is a corner
+     * @return (boolean): whether the BorderLine is a corner
+     */
     private boolean isCorner() {
         return getOrientation().equals(Orientation.TOP_LEFT)
                 || getOrientation().equals(Orientation.TOP_RIGHT)
@@ -293,11 +326,21 @@ public class BorderLine extends Region {
                 || getOrientation().equals(Orientation.BOTTOM_LEFT);
     }
 
+    /**
+     * Determines whether the {@link BorderLine} is a purely horizontal line<br>
+     * (ie): it is situated at the top or the bottom
+     * @return (boolean): whether the Borderline is a horizontal line
+     */
     private boolean isHorizontal() {
         return getOrientation().equals(Orientation.TOP)
                 || getOrientation().equals(Orientation.BOTTOM);
     }
 
+    /**
+     * Determines whether the {@link BorderLine} is a purely vertical line<br>
+     * (ie): it is situated to the right or the left
+     * @return (boolean): whether the BorderLine is a vertical line
+     */
     private boolean isVertical() {
         return getOrientation().equals(Orientation.RIGHT)
                 || getOrientation().equals(Orientation.LEFT);
@@ -365,106 +408,191 @@ public class BorderLine extends Region {
 
     /*     CSS STYLEABLE PROPERTIES     */
 
-    // size
+    /**
+     * Size {@link Property} for the {@link BorderLine}
+     * @return (DoubleProperty): the Borderline's size property
+     */
     public DoubleProperty sizeProperty() {
         return (DoubleProperty) size;
     }
 
+    /**
+     * Getter for the {@link BorderLine}'s size<br>
+     * (ie): its width & height
+     * @return (double): the Borderline's size
+     */
     public double getSize() {
         return size.getValue().doubleValue();
     }
 
+    /**
+     * Setter for the {@link BorderLine}'s size<br>
+     * (ie): its width & height
+     * @param newSize (double): the Borderline's new size
+     */
     public void setSize(final double newSize) {
+        // determines if a minimal width & height have already been set to the BorderLine...
         final boolean WIDTH_BOUNDS_SET = getMinWidth() != -1 && getMaxWidth() != -1;
+        // ...and if that is the case, makes sure that the Borderline's new size is smaller than its max size
         if (WIDTH_BOUNDS_SET && (newSize < getMinWidth() || newSize > getMaxWidth())) return;
 
+        // if the size is valid sets it
         size.setValue(newSize);
 
+        // updates the canvas' size if it has been initialised
         if (canvas != null) {
             canvas.setWidth(newSize);
             canvas.setHeight(newSize);
         }
 
+        // updates the Borderline's preferred size...
         setPrefSize(getSize(), getSize());
+        // ...and updates the canvas
         refreshDisplay();
     }
 
-    // thickness
+    /**
+     * Thickness {@link Property} for the {@link BorderLine}
+     * @return (DoubleProperty): the Borderline's thickness property
+     */
     public DoubleProperty thicknessProperty() {
         return (DoubleProperty) thickness;
     }
 
+    /**
+     * Getter for the {@link BorderLine}'s thickness
+     * @return (double): the BorderLine's line thickness
+     */
     public double getThickness() {
         return thickness.getValue().doubleValue();
     }
 
+    /**
+     * Setter for the {@link BorderLine}'s thickness
+     * @param newThickness (double): the Borderline's new thickness
+     */
     public void setThickness(final double newThickness) {
+        // verifies that the BorderLine's new thickness is within the bounds of its size
         if (newThickness < 0 || newThickness > getSize()) return;
 
+        // if the new thickness is valid, updates the BorderLines thickness to it...
         thickness.setValue(newThickness);
+        // ...and updates the canvas
         refreshDisplay();
     }
 
-    // arc width
+    /**
+     * Arc width {@link Property} for the {@link BorderLine}
+     * @return (DoubleProperty): the BorderLine's war width property
+     */
     public DoubleProperty arcWidthProperty() {
         return (DoubleProperty) arcWidth;
     }
 
+    /**
+     * Getter for the {@link BorderLine}'s arc width
+     * @return (double): the Borderline's arc width
+     */
     public double getArcWidth() {
         return arcWidth.getValue().doubleValue();
     }
 
+    /**
+     * Setter for the {@link BorderLine}'s arc width
+     * @param newArcWidth (double): new arc height for the BorderLine
+     */
     public void setArcWidth(final double newArcWidth) {
+        // checks the arc width is valid...
         if (newArcWidth < 0) return;
+        // ...and if that is the case applies it...
         arcWidth.setValue(newArcWidth);
+        // ...and updates the canvas
         refreshDisplay();
     }
 
-    // arc height
+    /**
+     * Arc height {@link Property} for the {@link BorderLine}
+     * @return (DoubleProperty): the {@link BorderLine}'s arc height property
+     */
     public DoubleProperty arcHeightProperty() {
         return (DoubleProperty) arcHeight;
     }
 
+    /**
+     * Getter for the {@link BorderLine}'s arc height
+     * @return (double): the Borderline's arc height
+     */
     public double getArcHeight() {
         return arcHeight.getValue().doubleValue();
     }
 
+    /**
+     * Setter for the {@link BorderLine}'s arc height
+     * @param newArcHeight (double): the BorderLine's new arc height
+     */
     public void setArcHeight(final double newArcHeight) {
+        // checks the new arc height is valid...
         if (newArcHeight < 0) return;
+        // ...if so applies it...
         arcHeight.setValue(newArcHeight);
+        // ...and updates the canvas
         refreshDisplay();
     }
 
-    // color
+    /**
+     * {@link Color} {@link Property} for the {@link BorderLine}
+     * @return (ObjectProperty(Color)): the BorderLine's color property
+     */
     public ObjectProperty<Color> colorProperty() {
         return (ObjectProperty<Color>) color;
     }
 
+    /**
+     * Getter for the {@link BorderLine}'s {@link Color}
+     * @return (Color): the BorderLine's Color
+     */
     public Color getColor() {
         return color.getValue();
     }
 
+    /**
+     * Setter for the {@link BorderLine}'s {@link Color}
+     * @param newColor ({@link Color}): the BorderLine's new color
+     */
     public void setColor(final Color newColor) {
-        if (newColor == null) {
-            color.setValue(DEFAULT_COLOR);
-        } else {
-            color.setValue(newColor);
-        }
+        // checks that the color is valid, if so sets it as the BorderLine's new color...
+        color.setValue(Objects.requireNonNullElse(newColor, DEFAULT_COLOR));
+
+        // ...and updates the canvas
         refreshDisplay();
     }
 
-    // orientation
+    /**
+     * {@link Orientation} {@link Property} for the {@link BorderLine}
+     * @return (ObjectProperty(Orientation)): the BorderLine's orientation property
+     */
     public ObjectProperty<Orientation> orientationProperty() {
         return orientation;
     }
 
+    /**
+     * Getter for the {@link BorderLine}'s {@link Orientation}
+     * @return (Orientation): the BorderLine's orientation
+     */
     public Orientation getOrientation() {
         return orientation.get();
     }
 
+    /**
+     * Setter for the {@link BorderLine}'s {@link Orientation}
+     * @param newOrientation ({@link Orientation}): the Borderline's new orientation
+     */
     public void setOrientation(Orientation newOrientation) {
+        // updates the Borderline's orientation
         orientation.set(newOrientation);
+        // recalculates where to draw the Borderline's rectangle...
         determineRectangleCoordinates(newOrientation);
+        // ...and updates the canvas
         refreshDisplay();
     }
 
--- src/main/java/app/customControls/controls/corner/Orientation.java
+++ src/main/java/app/customControls/controls/corner/Orientation.java
@@ -1,7 +1,19 @@
 package app.customControls.controls.corner;
 
 /**
- * Represents the orientation of a {@link javafx.scene.Node}
+ * Represents the orientation of a {@link javafx.scene.Node}<br>
+ * <br>
+ * <u><i>Values</i></u> :<br>
+ * <ul>
+ *     <li>TOP_LEFT</li>
+ *     <li>TOP</li>
+ *     <li>TOP_RIGHT</li>
+ *     <li>RIGHT</li>
+ *     <li>BOTTOM_RIGHT</li>
+ *     <li>BOTTOM</li>
+ *     <li>BOTTOM_LEFT</li>
+ *     <li>LEFT</li>
+ * </ul>
  * @see BorderLine
  */
 public enum Orientation {
